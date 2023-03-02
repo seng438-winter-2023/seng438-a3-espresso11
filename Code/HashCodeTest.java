799c992073a20c1c505030d000612c5323bb292e
Room <108599236+codeongaku25@users.noreply.github.com>
2023-03-01 19:43:05 -0700

    Update HashCodeTest.java

--- Code/HashCodeTest.java
+++ Code/HashCodeTest.java
@@ -12,14 +12,28 @@ public class hashCodeTest {
 	
 	@Before
 	public void setUp() throws Exception {
+		x = new String("String");
+	        y = new String("String");
 	}
 	
 	@Test
-  
-  
-  @After
-  public void tearDown() throws Exception {
-  }
+	public void testReflexiveEquality() {
+	// for any non-null reference value x, x.equals(x) should return true
+		assertTrue(x.equals(x));
+		assertTrue(x.hashCode() == x.hashCode());
+	}
+	
+	@Test
+	public void testSymmetricEquality() {
+	/* for any non-null reference values x and y, x.equals(y) should return true
+	if and only if y.equals(x) returns true */
+		assertTrue(x.equals(y) && y.equals(x));
+		assertTrue(x.hashCode() == y.hashCode());
+	}
+    
+        @After
+        public void tearDown() throws Exception {
+        }
     
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
