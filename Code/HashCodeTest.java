ffff14e47c98f866ab031e49af74fb8f8984e55e
Room <108599236+codeongaku25@users.noreply.github.com>
2023-03-01 19:02:43 -0700

    Update and rename HashCodeTesting.java to HashCodeTest.java

---
+++ Code/HashCodeTest.java
@@ -0,0 +1,27 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+import org.jfree.data.Range;
+import org.junit.*;
+
+public class hashCodeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+  
+  
+  @After
+  public void tearDown() throws Exception {
+  }
+    
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+}
